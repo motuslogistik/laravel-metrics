8fb96614c5fc860b52998c32a33b7b9d3726a350
Jon Gotlin <jon@jon.se>
2026-08-31 10:24:20 +0200

    Test fail

---
+++ tests/CanaryTest.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Temporary: deliberate test failure to verify GitHub PR annotations. Delete me.
+ */
+it('fails on purpose to check CI annotations', function () {
+    expect(1 + 1)->toBe(3);
+});
