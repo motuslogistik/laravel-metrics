8e1e2f5878437d6767513dbdb1729164596de14a
Jon Gotlin <jon@jon.se>
2026-08-31 10:29:46 +0200

    Make it green

--- tests/CanaryTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * Temporary: deliberate test failure to verify GitHub PR annotations. Delete me.
- */
-it('fails on purpose to check CI annotations', function () {
-    expect(1 + 1)->toBe(3);
-});
